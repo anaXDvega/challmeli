d4d97b30e7f462ba9586bea021fb73d72c3ea319
Ana Vega <40269205+anaXDvega@users.noreply.github.com>
2022-04-09 10:02:25 -0500

    llamado de findAll

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -5,4 +5,11 @@ fetch('http://localhost:8080/registryIp/', valorIp)
       .then(response => response.json())
       .then(json => console.log(json))
 }
+function getData(){
+    console.log("Funcion cargada al inicio");
+       fetch('http://localhost:8080/findAll')
+      .then(response => response.json())
+      .then(json => console.log(json))
+}
+
 window.onload = getData();
