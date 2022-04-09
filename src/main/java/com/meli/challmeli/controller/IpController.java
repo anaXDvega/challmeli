b1bf2721ea067d904a1826dcf59d1ebd0ff00261
Ana Vega <40269205+anaXDvega@users.noreply.github.com>
2022-04-09 11:26:58 -0500

    llamado de vista

--- src/main/java/com/meli/challmeli/controller/IpController.java
+++ src/main/java/com/meli/challmeli/controller/IpController.java
@@ -1,9 +1,9 @@
 package com.meli.challmeli.controller;
+import com.meli.challmeli.model.Distance;
 import com.meli.challmeli.service.InfoIpService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class IpController {
@@ -11,8 +11,19 @@ public class IpController {
     @Autowired
     InfoIpService infoIpService;
 
+    @GetMapping("/main")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "main.js";
+    }
+
     @GetMapping("/registryIp/{ip}")
     public Object registryIp(@PathVariable String ip) {
           return infoIpService.countryInfoComplete(ip);
+    }
+
+    @GetMapping("/findAll")
+    public Iterable<Distance> findAll() {
+        return infoIpService.findAllDistance();
     }
 }
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,15 +1,27 @@
 function consultarIp(){
-var valorIp = document.getElementById('inputIp');
+var valorIp = document.getElementById('inputIp').value;
 console.log("consultarIp" + valorIp);
-fetch('http://localhost:8080/registryIp/', valorIp)
-      .then(response => response.json())
-      .then(json => console.log(json))
+const url = 'http://localhost:8080/registryIp/190.173.136.0';
+async function fetchAPI(apiURL) {
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
 }
+fetchAPI(url).then(data => {
+  console.log('Toda la info: ', data);
+}).catch(error => {console.log(error)});//en caso de algún error
+  }
 function getData(){
     console.log("Funcion cargada al inicio");
-       fetch('http://localhost:8080/findAll')
-      .then(response => response.json())
-      .then(json => console.log(json))
+    const url = '/findAll';
+    async function fetchAPI(apiURL) {
+      let response = await fetch(url);
+      let data = await response.json();
+      return data;
+    }
+    fetchAPI(url).then(data => {
+      console.log('Toda la info: ', data);
+    }).catch(error => {console.log(error)});//en caso de algún error
 }
 
 window.onload = getData();
